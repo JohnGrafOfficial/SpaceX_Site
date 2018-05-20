7cf401798057d2e3e4675a60d78c37d299362585
AbductionGame <abductiongame@gmail.com>
2018-05-19 20:56:35 -0500

    Add project files.

---
+++ SpaceXWebsite/Launches.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Launches.aspx.cs" Inherits="SpaceXWebsite.Launches" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <style>
+        #launchesTable
+        {
+            margin:auto;
+            padding: 8px;
+            width: 80%;
+            font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+        #launchesTable tr:nth-child(even)
+        {
+            background-color: #f2f2f2;
+
+        }
+
+        #launchesTable th
+        {
+            background-color: #181c1f;
+            color:white;
+
+        }
+
+        h1{
+            text-align:center;
+            font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
+
+        h3{
+            text-align:center;
+            font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
+
+
+
+
+    </style>
+    <title>SpaceX Launches</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>SpaceX Launch Data</h1>
+        <h3>2006 - Present</h3>
+
+            <asp:Table runat="server" id="launchesTable" >
+                <asp:TableHeaderRow>
+                    <asp:TableHeaderCell>Launch Date</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Rocket Name</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Cargo Manifest</asp:TableHeaderCell>
+                    <asp:TableHeaderCell>Success</asp:TableHeaderCell>
+                </asp:TableHeaderRow>
+            </asp:Table>
+
+        <br /><p>Made by John Graf, 2018</p>
+    
+    </form>
+</body>
+</html>
---
+++ SpaceXWebsite/Launches.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace SpaceXWebsite
+{
+    //root class for launch
+    public class Launch
+    {
+        public String launch_date_local { get; set; }
+        public String mission_name { get; set; }
+        public String launch_success { get; set; }
+
+        public Rocket rocket { get; set; }
+        public Links links { get; set; }
+
+    }
+
+    //rocket data
+    public class Rocket
+    {
+        public String rocket_name { get; set; }
+    }
+
+    //launch links
+    public class Links
+    {
+        public String presskit { get; set; }
+    }
+
+    public partial class Launches : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Creating the request
+            String url = "https://api.spacexdata.com/v2/launches/";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            //Sending request to API
+            var response = (HttpWebResponse)request.GetResponse();
+
+            //Reading response
+            string content = string.Empty;
+            using (var stream = response.GetResponseStream())
+            {
+                using(var sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+
+            //Turning JSON into an array of launches
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Launch[] launches = js.Deserialize<Launch[]>(content);
+
+
+            //Outputting retrieved data into html table
+            for (int i = 0; i < launches.Length; i++)
+            {
+                TableRow launch = new TableRow();
+                launchesTable.Rows.Add(launch);
+
+                TableCell dateCell = new TableCell();
+                DateTime lDate = DateTime.Parse(launches[i].launch_date_local);
+                dateCell.Text = lDate.ToString("MM/dd/yyyy HH:mm");
+                launch.Cells.Add(dateCell);
+
+                TableCell nameCell = new TableCell();
+                nameCell.Text = launches[i].rocket.rocket_name;
+                launch.Cells.Add(nameCell);
+
+                TableCell cargoCell = new TableCell();
+                cargoCell.Text = "<a href =\"" +launches[i].links.presskit + "\">" + launches[i].links.presskit + "</a>";
+                launch.Cells.Add(cargoCell);
+
+                TableCell successCell = new TableCell();
+                successCell.Text = launches[i].launch_success;
+                launch.Cells.Add(successCell);
+            }
+        }
+    }
+}
